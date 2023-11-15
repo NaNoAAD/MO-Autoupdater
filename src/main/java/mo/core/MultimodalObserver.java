d43ceab053b554919f643bdf7d52589419764440
NaNoAAD <hernan.aranda@usach.cl>
2023-11-14 23:26:36 -0300

    Punto de restauracion para pruebas de reemplazo archivos

--- src/main/java/mo/core/MultimodalObserver.java
+++ src/main/java/mo/core/MultimodalObserver.java
@@ -27,7 +27,6 @@ public class MultimodalObserver {
         
         PluginRegistry.getInstance();
         
-        
 
         MainWindow window = new MainWindow();
         MainPresenter presenter = new MainPresenter(window);
