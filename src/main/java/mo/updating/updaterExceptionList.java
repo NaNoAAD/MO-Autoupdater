0c482d740abed8bfb599644aa96456f59660ae7d
NaNoAAD <hernan.aranda@usach.cl>
2023-12-07 00:30:49 -0300

    Creacion de un diccionario de excepciones
    Primeros pasos para el uso de un diccionario de excepciones de nombres en los archivos, para evitar la saturacion de codigo en el if principal de la creacion del registro local

---
+++ src/main/java/mo/updating/updaterExceptionList.java
@@ -0,0 +1,18 @@
+package mo.updating;
+
+/**
+ * Clase que sirve para contener las palabras que los registros de archivos deben evitar obtener
+ */
+public class updaterExceptionList {
+    
+    public static final String[] fileNamesToAvoid = {"java\\mo\\updating",
+    "Register.txt",
+    "RemoteRegister.txt",
+    "FileRegister.txt",
+    "Repo.zip",
+    ".gradle",
+    ".git",
+    "\\bin",
+    "\\build"
+    };
+}
